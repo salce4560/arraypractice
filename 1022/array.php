f61635257f623719672edc0764ebafdc09108e7d
salce <s22464580s@gmail.com>
2021-10-22 12:02:08 +0800

    array

--- 1022/array.php
+++ 1022/array.php
@@ -1,6 +1,19 @@
 <?php
-$a=[[1,2,3,4,5,6],[4,3,1,0],[1,5,4,5]];
-echo"$a[0],[0],[1]";
+
+$name=[];
+$subject=['國文','英文','數學','地理','歷史'];
+$name['judy']=[95,64,70,90,84];
+$name['amo']=[95,64,70,90,84];
+$name['john']=[95,64,70,90,84];
+$name['peter']=[95,64,70,90,84];
+$name['hebe']=[95,64,70,90,84];
+echo"<pre>";
+print_r($name);
+print_r($subject);
+echo"</pre>";
+print_r($name['judy']);
+
+echo"judy的數學成績".$name['judy'][2]."分";
 
 
 ?>
